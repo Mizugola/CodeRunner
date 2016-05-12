984eeae301b79a53b903d649c60f7bb874a69864
Nyckel <mathis.ouarnier@gmail.com>
2016-05-12 11:48:42 +0200

    Mise a jour arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -4,7 +4,6 @@
   const int rc2 = 3;
   const int rc4 = 4;
   const int rc8 = 5;
-  //const int rc16 =6;
   
   int combi = 0;
   
@@ -15,10 +14,8 @@ void setup() {
   pinMode(rc2, INPUT_PULLUP); 
   pinMode(rc4, INPUT_PULLUP);
   pinMode(rc8, INPUT_PULLUP);
- // pinMode(rc16, INPUT_PULLUP);
   
   Serial.begin(9600);
-  Serial.println("Arduino ready to work");
 }
 
 void loop() {
@@ -27,7 +24,7 @@ void loop() {
   if(digitalRead(rc2)==LOW)combi+=2;
   if(digitalRead(rc4)==LOW)combi+=4;
   if(digitalRead(rc8)==LOW)combi+=8;
-  //if(digitalRead(rc16)==LOW)combi+=16;
+
   
   //if(combi!=0){ Décommenter si on ne veut rien recevoir quand aucun objet sur le socle
     if(Serial.available()==0) Serial.println(combi); //L'arduino n'a rien reçu->peut envoyer
